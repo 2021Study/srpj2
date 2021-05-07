45ea4f30100280a887219501027b825843ef5f45
jimin0127 <ckdgh4662@naver.com>
2021-05-07 14:25:28 +0900

    mrgo-backend 프로젝트 생성

---
+++ margo-backend/src/main/java/com/mrgo/mrgobackend/MrgoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrgo.mrgobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrgoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrgoBackendApplication.class, args);
+    }
+
+}
---
+++ margo-backend/src/test/java/com/mrgo/mrgobackend/MrgoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrgo.mrgobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrgoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
